e563a0168fdb865e570e282d828dfd8e0070a8c4
Jacob martin <jacobmartinsmiltth@gmail.com>
2024-08-19 23:52:26 +0100

    cart 1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import { useEffect, useRef } from "react";
-import { AppDispatch, RootState } from "./Redux/store";
+import { AppDispatch } from "./Redux/store";
 import { useDispatch } from "react-redux";
 import { setActiveMenu } from "./Redux/features/activeMenuSlice";
 import useInView from "./components/useInView";
@@ -8,7 +8,6 @@ import Home from "./components/Home/Home";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Shop from "./components/shop/Shop";
 import Cart from "./components/Cart";
-import { useSelector } from "react-redux";
 
 const App = () => {
   const productsRef = useRef(null);
@@ -20,7 +19,6 @@ const App = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const currentPath = location.pathname;
-  const showCart = useSelector((state: RootState) => state.data.cart.showCart);
 
 
   const inView = useInView([homeRef, productsRef, reviewRef, aboutRef], 0.3);
